0fffa7dc946c30abbff65848991d1da0b01bf70b
NigoraMamad <nigoramamdv@gmail.com>
2025-05-21 17:50:58 +0200

    sllep challnege by week

---
+++ CBT-Insomnia/model/SleepChallange.swift
@@ -0,0 +1,38 @@
+//
+//  SleepChallange.swift
+//  CBT-Insomnia
+//
+//  Created by Nigorakhon Mamadalieva on 21/05/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class SleepChallenge {
+    var id: UUID
+    var title: String
+    var startDate: Date
+    var endDate: Date
+
+    var minEfficiencyGoal: Double
+
+
+    // Relationship: one-to-many
+    @Relationship(deleteRule: .nullify, inverse: \SleepSession.challenge)
+    var sleepSessions: [SleepSession] = []
+
+    init(
+        id: UUID = UUID(),
+        title: String,
+        startDate: Date,
+        endDate: Date,
+        minEfficiencyGoal: Double
+    ) {
+        self.id = id
+        self.title = title
+        self.startDate = startDate
+        self.endDate = endDate
+        self.minEfficiencyGoal = minEfficiencyGoal
+    }
+}
