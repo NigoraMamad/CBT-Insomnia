930bcd10913d3228b1e909531bf28a29c47fe875
Michele Coppola <michele.coppola17@outlook.it>
2025-05-23 15:17:25 +0200

    added MeshGradient, card layout, and glass material

---
+++ CBT-Insomnia/view/CardView.swift
@@ -0,0 +1,28 @@
+//
+//  CardView.swift
+//  CBT-Insomnia
+//
+//  Created by Michele Coppola on 22/05/25.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var height: CGFloat
+    
+    var body: some View {
+        ZStack {
+            MeshGradientBg()
+            
+            RoundedRectangle(cornerRadius: 30)
+                .frame(height: height)
+                .padding()
+                .foregroundStyle(.ultraThinMaterial.opacity(0.75))
+        }
+        
+    }
+}
+
+#Preview {
+    CardView(height: 400)
+}
---
+++ CBT-Insomnia/view/LastNightCardView.swift
@@ -0,0 +1,63 @@
+//
+//  LastNightCardView.swift
+//  CBT-Insomnia
+//
+//  Created by Michele Coppola on 22/05/25.
+//
+
+import SwiftUI
+
+struct LastNightCardView: View {
+    var body: some View {
+        ZStack {
+            Color.indigo
+            CardView(height: 400)
+            VStack {
+                HStack {
+                    HStack {
+                        Text("MON")
+                            .bold()
+                            .font(.largeTitle)
+                        VStack {
+                            Text("19TH")
+                                .font(.caption)
+                                .fontWeight(.medium)
+                            Text("MAY")
+                                .font(.caption)
+                                .fontWeight(.medium)
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    Text("70%")
+                        .bold()
+                        .font(.largeTitle)
+                }
+                .padding(40)
+                
+                Circle()
+                    .frame(width: 150)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("you slept")
+                            .font(.callout)
+                        Text("5h 30m")
+                            .bold()
+                            .font(.title)
+                    }
+                    
+                    Spacer()
+                }
+                .padding(40)
+            }
+        }
+        .foregroundStyle(.white)
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    LastNightCardView()
+}
